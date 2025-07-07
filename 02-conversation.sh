c37c81fac056b2f9ea569f68793d9336c35930b6
manikumar <manisurarapu24@gamil.com>
2025-07-07 17:26:34 +0530

    hello world started

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-Mani :: Hi Mark, How are yoy?
-Mark :: Hello Mani, I am fine, Thanks for asking, How are you Mani?
-Mani :: I am good too. Thanks, and where are you?
-Mark :: I am in India, and I am going to leave India next month.
-Mani :: Hoo great! 
+echo "Mani: Hello Mark!"
+echo "Mark: Hi Mani, how are you?"
+echo "Mani: I'm learning shell scripting."
+echo "Mark: That's awesome!"
+echo "Mani: Let's build something cool."
