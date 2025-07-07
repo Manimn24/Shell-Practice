66601243e047f7a6d2303850649fc14bb2897f3f
manikumar <manisurarapu24@gamil.com>
2025-07-08 04:32:53 +0530

    hello world started

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -2,3 +2,8 @@
 
 echo "All variables passed to the script : $@"
 echo "Number of variables : $#"
+echo "Home User : $Home"
+ech0 "Which user : $user"
+echo "file name : $0"
+echo "PID in script : $$"
+echo "PID of last command running in background : $!"
