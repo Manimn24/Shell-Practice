fd61067b72575b96b3c5cbf1ec7b88b4a5fdebdc
manikumar <manisurarapu24@gamil.com>
2025-07-07 17:17:27 +0530

    hello world started

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+Mani :: Hi Mark, How are yoy?
+Mark :: Hello Mani, I am fine, Thanks for asking, How are you Mani?
+Mani :: I am good too. Thanks, and where are you?
+Mark :: I am in India, and I am going to leave India next month.
+Mani :: Hoo great! 
