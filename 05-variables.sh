39fc56e88bcea069bff96aff9e5310985eeb716d
manikumar <manisurarapu24@gamil.com>
2025-07-07 18:36:37 +0530

    hello world started

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+Person1=Mani
+Person2=Mark
+
+echo "$person1:: Hello $person2!"
+echo "$person2:: Hi $person1, how are you?"
+echo "$person1:: I'm learning shell scripting."
+echo "$person2:: That's awesome!"
+echo "$person1:: Let's build something cool."
+echo "$person2:: Sure, Thanks."
