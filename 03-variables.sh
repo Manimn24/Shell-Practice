43eeda0199c7ce432438fbb3ff6ec9c56cf49f5e
manikumar <manisurarapu24@gamil.com>
2025-07-07 17:51:10 +0530

    02-conversation.sh

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,8 +3,8 @@
 Person1=Mani
 Person2=Mark
 
-echo "$person1: Hello $person2!"
-echo "$person2: Hi $person1, how are you?"
-echo "$person1: I'm learning shell scripting."
-echo "$person2: That's awesome!"
-echo "$person1: Let's build something cool."
+echo "$person1:: Hello $person2!"
+echo "$person2:: Hi $person1, how are you?"
+echo "$person1:: I'm learning shell scripting."
+echo "$person2:: That's awesome!"
+echo "$person1:: Let's build something cool."
